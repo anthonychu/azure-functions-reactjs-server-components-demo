4066b52767fa5a56f3fe2a7d23b275c1a4c6995b
Anthony Chu <antchu@microsoft.com>
2020-12-31 15:45:28 -0800

    Make it smart about how it finds manifest entry

--- react/index.server.js
+++ react/index.server.js
@@ -1,33 +1,13 @@
 require('../funcutil/babelregister');
 
 const React = require('react');
-const {pipeToNodeWritable} = require('react-server-dom-webpack/writer.node.server');
+const { pipeToNodeWritable } = require('react-server-dom-webpack/writer.node.server');
 const ReactApp = require('../src/App.server').default;
-const {readFileSync} = require('fs');
+const { readFileSync } = require('fs');
 const path = require('path');
 
 module.exports = async function (context, req) {
-    const manifest = readFileSync(
-        path.resolve(__dirname, '../build/react-client-manifest.json'),
-        'utf8'
-    );
-    const moduleMapOrig = JSON.parse(manifest);
-
-    let moduleMap = moduleMapOrig;
-
-    // override the entries of the manifest if it's not in the original folder
-    if (process.env.REACT_MANIFEST_DIRECTORY) {
-        moduleMap = {};
-        for (const [key, value] of Object.entries(moduleMapOrig)) {
-            const filenameMatch = /\/([^/]+)$/.exec(key);
-            if (filenameMatch) {
-                console.log(filenameMatch[1]);
-                moduleMap[process.env.REACT_MANIFEST_DIRECTORY + filenameMatch[1]] = value;
-            }
-        }
-        console.log(moduleMap);
-    }
-
+    const moduleMap = getModuleMap();
     const location = JSON.parse(req.query.location);
 
     const props = {
@@ -35,20 +15,65 @@ module.exports = async function (context, req) {
         isEditing: location.isEditing,
         searchText: location.searchText,
     };
-    // const props = location;
-context.log(props)
+
     const writer = new Writer();
     return new Promise((resolve, reject) => {
         writer.on('done', (result) => resolve({
             status: 200,
             body: result,
-            headers: {'Content-Type': 'application/text', 'X-Location': JSON.stringify(props)}
+            headers: { 'Content-Type': 'application/text', 'X-Location': JSON.stringify(props) }
         }));
         pipeToNodeWritable(React.createElement(ReactApp, props), writer, moduleMap);
     });
 }
 
+let moduleMapCache = null;
+function getModuleMap() {
+    if (moduleMapCache) {
+        return moduleMapCache;
+    }
 
+    const manifest = readFileSync(
+        path.resolve(__dirname, '../build/react-client-manifest.json'),
+        'utf8'
+    );
+    const moduleMap = JSON.parse(manifest);
+
+    moduleMapCache = new Proxy(moduleMap, {
+        get: function (target, prop, receiver) {
+            if (target[prop]) {
+                return target[prop];
+            }
+
+            const scoredKeys = Object.keys(target)
+                .map(k => {
+                    // compare strings from end and return number of matching characters
+                    const keyChars = k.split('');
+                    const propChars = prop.split('');
+
+                    for (let i = 0; i < keyChars.length && i < propChars.length; i++) {
+                        console.log(`${propChars[propChars.length - i - 1]} = ${keyChars[keyChars.length - i - 1]}`)
+                        if (keyChars[keyChars.length - i - 1] !== propChars[propChars.length - i - 1]) {
+                            return {
+                                key: k,
+                                matchedChars: i
+                            };
+                        }
+                    }
+                    return {
+                        key: k,
+                        matchedChars: 0
+                    };
+                })
+                .sort((a, b) => b.matchedChars - a.matchedChars);
+
+            if (scoredKeys.length && scoredKeys[0].matchedChars) {
+                return target[scoredKeys[0].key];
+            }
+        }
+    });
+    return moduleMapCache;
+}
 
 class Writer {
     constructor() {
@@ -61,7 +86,10 @@ class Writer {
         this.buffer.push(s.toString())
         return true;
     }
-    end() {
+    end(s) {
+        if (s) {
+            this.write(s);
+        }
         console.log("DONE!!!!!")
         const handlers = this.handlers.done;
         if (handlers) {
